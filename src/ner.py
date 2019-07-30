cc805bfc94b8fa7dbeaf00d6020bd69b2bd5eb0c
geodge831012 <geodge831012@163.com>
2019-07-30 15:09:52 +0800

    ok

--- src/ner.py
+++ src/ner.py
@@ -13,12 +13,19 @@ class Ner():
         # 所有实体词集合
         self._ner_word_list = []
 
+        # 实体词替换的名字
+        self._ner_name = ""
+
         # AC模型的builder
         self._builder = AcoraBuilder()
 
     # 设置实体词集合
     def set_ner_word_list(self, ner_word_list):
         self._ner_word_list = ner_word_list
+
+    # 设置实体词替换的名字
+    def set_ner_name(self, ner_name):
+        self._ner_name = ner_name
 
     # 构建模型
     def build_ner(self):
@@ -31,7 +38,7 @@ class Ner():
     def hit(self, content_str):
         hit_list = []
         for hit_word, pos in self._tree.finditer(content_str):
-            hit_list.append([hit_word, pos])
+            hit_list.append([hit_word, pos, self._ner_name])
 
         return hit_list
 
@@ -59,6 +66,26 @@ class NerMgr():
 
         return hit_list
 
+    # 实体词对象的扩充函数
+    def fill_ner(self):
+        # 股票名称的实体词对象的填充
+        self.fill_ner_stock_name()
+
+    # 股票名称的实体词对象的填充
+    def fill_ner_stock_name(self):
+        # 从db获取股票名称
+        stock_name_list = ["格林美", "海康威视"]
+
+        ner = Ner()
+        ner.set_ner_word_list(stock_name_list)
+        ner.set_ner_name("股票名称")
+        ner.build_ner()
+
+        self.append_ner(ner)
+
+
+ner_mgr = NerMgr()
+ner_mgr.fill_ner()
 
 #########################################################################
 ## main 主函数 ##
